fe90a7b53b8245165fc51c5e366f1a6322cb59ff
cypher <jp10010101010000@gmail.com>
2016-09-10 09:57:27 +1000

    Fixed navbar and draw size

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -8,6 +8,7 @@ import './mainLayout.html';
 import './body.html';
 import './templates/templates.js';
 import './pages/pages.js';
+import './style.scss';
 
 Template.registerHelper('user', function() {
     return Meteor.user().username || Meteor.user().profile.name;
