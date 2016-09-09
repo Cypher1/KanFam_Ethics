9419fcd7e43f5aa4b3aedd073f415375f817f1b5
cypher <jp10010101010000@gmail.com>
2016-09-09 19:42:13 +1000

    Added login/out routing functions

--- imports/startup/router.js
+++ imports/startup/router.js
@@ -1,8 +1,60 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
+
+function checkLoggedIn (ctx) {
+    // Redirect unloggedin users
+    if (!Meteor.userId()) {
+        FlowRouter.go('/')
+    }
+}
+
+Accounts.onLogin(function () {
+    // Send the user to their dashboard
+    FlowRouter.go('dashboard')
+})
+
+Accounts.onLogout(function () {
+    // Send the user home
+    FlowRouter.go('home')
+})
+
+// PUBLIC ROUTES
+
 FlowRouter.route('/', {
-  action: function() {
-    BlazeLayout.render("mainLayout", {content: "landingpage"});
-  }
+    name: 'home',
+    action: function() {
+        BlazeLayout.render("mainLayout", {content: "landing"});
+    }
+});
+
+FlowRouter.route('/login', {
+    name: 'login',
+    action: function() {
+        if(!Meteor.userId()) {
+            BlazeLayout.render("mainLayout", {content: "login"});
+        } else {
+            FlowRouter.go('/');
+        }
+    }
+});
+
+// PRIVATE ROUTES
+var privateRoutes = FlowRouter.group({
+    name: 'private',
+    triggersEnter: [checkLoggedIn]
+})
+
+privateRoutes.route('/dashboard', {
+    name: 'dashboard',
+    action: function () {
+        BlazeLayout.render('mainLayout', {content: 'dashboard'})
+    }
+})
+
+privateRoutes.route('/logout', {
+    name: 'logout',
+    action: function() {
+        Meteor.logout();
+    }
 });
