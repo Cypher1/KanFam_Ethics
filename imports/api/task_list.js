44f494e91353a8893345ded748b006d94f44739f
cypher <jp10010101010000@gmail.com>
2016-09-15 12:18:42 +1000

    Moved around some templates, added their js. Also worked on Group info template

---
+++ imports/api/task_list.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+
+export const TaskList = new Mongo.Collection('task_list');
+
+if (Meteor.isServer) {
+    // This code only runs on the server
+    Meteor.publish('task_list', function groupsPublication() {
+        let publicGroups = {private: {$ne: true}};
+        if(!this.userId) {
+            return Groups.find(publicGroups);
+        }
+        let myGroups = {owner: this.userId};
+        return Groups.find({$or: [myGroups, publicGroups]});
+    });
+}
--- imports/ui/templates/templates.js
+++ imports/ui/templates/templates.js
@@ -1,5 +1,7 @@
 import './navbar.js';
-import './navlist.html';
-import './task.html';
-import './task.js';
 import './footer.html';
+import './login_signup.js';
+import './task.js';
+import './task_list.js';
+import './group.js';
+import './group_page.js';
