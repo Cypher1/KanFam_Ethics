6970fce015f80c6f8b342f84d8d5c3341f9e45d2
cypher <jp10010101010000@gmail.com>
2016-10-08 18:55:41 +1100

    Added groups api to server

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 // Import API js
 import '../imports/api/tasks.js';
+import '../imports/api/groups.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
