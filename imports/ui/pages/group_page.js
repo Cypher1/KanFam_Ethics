9bc2832b140e0cb75629784a5c9476866262979a
Erik Zhong <z3462813@student.unsw.edu.au>
2016-10-10 10:57:51 +1100

    Added lists to the group page

--- imports/ui/pages/group_page.js
+++ imports/ui/pages/group_page.js
@@ -2,23 +2,45 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 import { Groups } from '../../api/groups.js';
 
 import "./group_page.html";
+import "../templates/all_lists.js";
 
 Template.group_page.onCreated(function () {
     Meteor.subscribe('groups');
 });
 
 Template.group_page.helpers({
-    get_group: function () {
-        return Groups.find({_id: FlowRouter.current().params._id});
-    },
 
-    name: function(){
-    	return Groups.find({_id: FlowRouter.current().params._id}, {name:1})
-    }
 });
 
 Template.group_page.onRendered(function () {
 });
 
 Template.group_page.events({
+	'submit .add-members'(event){
+		// Prevent default browser from submit
+		event.preventDefault();
+
+		// Get values from the form
+		var members = event.target.members.value;
+
+		// Add new members into the group's database
+		Meteor.call('groups.add_member');
+
+		// Clear the form
+		event.target.members.value = '';
+	},
+	'submit .setIcon'(event){
+		// Prevent default browser from submit
+		event.preventDefault();
+
+		// Get the group icon
+		var icon = event.target.icon.value;
+
+		// Convert to base64 to store on the database
+		// TO DO: find a way to convert
+
+		//Insert icon into database
+		//Meteor.call('groups.')
+
+	}
 });
