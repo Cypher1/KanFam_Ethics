053f7de39be4822551c33d1ff0dfeb27c1e4150e
Erik Zhong <z3462813@student.unsw.edu.au>
2016-10-09 18:08:16 +1100

    added create group form

--- imports/ui/pages/group_page.js
+++ imports/ui/pages/group_page.js
@@ -8,27 +8,11 @@ Template.group_page.onCreated(function () {
 });
 
 Template.group_page.helpers({
-    get_group: function () {
-        return Groups.findOne({_id: FlowRouter.current().params._id});
-    }
+    
 });
 
 Template.group_page.onRendered(function () {
 });
 
 Template.group_page.events({
-    'submit .new-group'(event) {
-        /* Prevent default browser form submit */
-        event.preventDefault();
-
-        /* Get value from form element */
-        const target = event.target;
-        const group_name = target.group_name.value;
-
-        /* Insert a task into the collection */
-        Meteor.call('groups.add_group', group_name);
-
-        /* Clear form */
-        target.group_name.value = '';
-    },
 });
---
+++ imports/ui/templates/all_groups.js
@@ -0,0 +1,41 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { Groups } from '../../api/groups.js';
+
+import './all_groups.html';
+import './group.js';
+import '../../api/groups.js';
+
+Template.registerHelper('user', function() {
+    return Meteor.user().username || Meteor.user().profile.name;
+});
+
+Template.all_groups.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+    Meteor.subscribe('group');
+});
+
+Template.all_groups.helpers({
+    get_group: function () {
+        return Groups.findOne({_id: FlowRouter.current().params._id});
+    }
+});
+
+Template.all_groups.events({
+	'submit .new-group'(event) { 
+        /* Prefent default browser from submit */
+		event.preventDefault();
+
+        /* Get values from form element */       
+		const groupName = event.target.groupName.value;
+		const groupDes = event.target.groupDes.value;
+
+        /* Insert a group into the collection */
+		Meteor.call('groups.add_group', groupName, groupDes);
+
+        /* Clear form */
+		event.target.groupName.value = '';
+		event.target.groupDes.value = '';
+ 	},
+});
