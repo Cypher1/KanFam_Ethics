052b7d2aab73b9b45722ee7a2c3f6658f371ae3b
Erik Zhong <z3462813@student.unsw.edu.au>
2016-10-10 10:24:52 +1100

    removed unnecessary templates

--- imports/ui/templates/group_list.js
+++
@@ -1,4 +0,0 @@
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { Groups } from '../../api/groups.js';
-
-import "./group_list.html";
--- imports/ui/templates/templates.js
+++ imports/ui/templates/templates.js
@@ -11,5 +11,3 @@ import './mysocial.js';
 import './myOauth.html';
 import './myOauth.js';
 import './all_groups.js';
-import './all_group_lists.js';
-import './group_list.js';
