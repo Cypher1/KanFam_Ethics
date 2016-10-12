62e7be15fc381b07bb0577b63f67349c089841e2
kat333 <katherine.perd@gmail.com>
2016-10-13 00:52:24 +1100

    added tasks due on given day to the personal card and experimented with ui colours and spacing

--- imports/ui/templates/user_info.js
+++ imports/ui/templates/user_info.js
@@ -1,23 +1,35 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 import { TaskList } from '../../api/task_list.js';
+import { Tasks } from '../../api/tasks.js';
 import { Meteor } from 'meteor/meteor';
 
 import './all_lists.js';
 import './task_list.js';
 import './user_info.html';
+import './task.js';
 import '../../api/task_list.js';
+import '../../api/tasks.js';
 
 Template.registerHelper('user', function() {
     return Meteor.user().username || Meteor.user().profile.name;
 });
 
 Template.user_info.onCreated(function bodyOnCreated() {
-    this.state = new ReactiveDict();
     Meteor.subscribe('task_list');
+    Meteor.subscribe('tasks');
+   //  Meteor.subscribe('tasks');
 });
 
+
 Template.user_info.helpers({
+
+  taskDue() {
+        date = new Date();
+        date = date.toISOString().slice(0,10);
+        console.log(date);
+        return Tasks.find({owner: Meteor.userId(), dueDate: date}).fetch();
+    },
 	user_email() {
      return Meteor.user().email;
 	},
@@ -34,7 +46,7 @@ Template.user_info.helpers({
       var dueDate = new Date();
       return dueDate.toISOString().slice(0,10);
       
-    }
+    },
 });
 
 
