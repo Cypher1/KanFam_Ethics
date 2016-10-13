387799691fe0c0a2feacbbcb119fa8a752ef9713
kat333 <katherine.perd@gmail.com>
2016-10-13 12:20:02 +1100

    fixed issue with date format timezones

--- imports/ui/templates/user_info.js
+++ imports/ui/templates/user_info.js
@@ -18,17 +18,20 @@ Template.registerHelper('user', function() {
 Template.user_info.onCreated(function bodyOnCreated() {
     Meteor.subscribe('task_list');
     Meteor.subscribe('tasks');
-   //  Meteor.subscribe('tasks');
 });
 
 
 Template.user_info.helpers({
 
+   timeFix (date) {
+    var local = new Date(date);
+    local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+  },
   taskDue() {
-        date = new Date();
-        date = date.toISOString().slice(0,10);
-        console.log(date);
-        return Tasks.find({owner: Meteor.userId(), dueDate: date}).fetch();
+      var d = new Date();
+      var today = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
+      return Tasks.find({owner: Meteor.userId(), dueDate: today}).fetch();
     },
 	user_email() {
      return Meteor.user().email;
@@ -43,8 +46,9 @@ Template.user_info.helpers({
     },
     currentDate(){
 
-      var dueDate = new Date();
-      return dueDate.toISOString().slice(0,10);
+      var d = new Date();
+      var dueDate = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
+      return dueDate;
       
     },
 });
