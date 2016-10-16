152b084544164ca2eb17b38497eeb3861aafed55
cypher <jp10010101010000@gmail.com>
2016-10-17 01:18:00 +1100

    Cleaned up random newlines, extra variables etc

--- imports/ui/templates/all_groups.js
+++ imports/ui/templates/all_groups.js
@@ -7,38 +7,30 @@ import './all_groups.html';
 import './group.js';
 import '../../api/groups.js';
 
-Template.registerHelper('user', function() {
-    return Meteor.user().username || Meteor.user().profile.name;
-});
-
 Template.all_groups.onCreated(function bodyOnCreated() {
-    this.state = new ReactiveDict();
-    Meteor.subscribe('groups');
+  Meteor.subscribe('groups');
 });
 
 Template.all_groups.helpers({
-
-    groups () {
-        const instance = Template.instance();//stores current instance of template
-        let filter = {};
-        return Groups.find();
-    },
+  groups () {
+    return Groups.find();
+  },
 });
 
 Template.all_groups.events({
-	'submit .new-group'(event) { 
-        /* Prefent default browser from submit */
-		event.preventDefault();
+  'submit .new-group'(event) {
+    /* Prefent default browser from submit */
+    event.preventDefault();
 
-        /* Get values from form element */       
-		const groupName = event.target.groupName.value;
-		const groupDes = event.target.groupDes.value;
+    /* Get values from form element */
+    const groupName = event.target.groupName.value;
+    const groupDes = event.target.groupDes.value;
 
-        /* Insert a group into the collection */
-		Meteor.call('groups.add_group', groupName, groupDes);
+    /* Insert a group into the collection */
+    Meteor.call('groups.add_group', groupName, groupDes);
 
-        /* Clear form */
-		event.target.groupName.value = '';
-		event.target.groupDes.value = '';
- 	},
+    /* Clear form */
+    event.target.groupName.value = '';
+    event.target.groupDes.value = '';
+  },
 });
