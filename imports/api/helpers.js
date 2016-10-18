67c1307ea81d92c0d6f363a9b3329ebbf27cba82
cypher <jp10010101010000@gmail.com>
2016-10-19 03:03:38 +1100

    Global user_identifier returns null instead of an error when logged out

--- imports/api/helpers.js
+++ imports/api/helpers.js
@@ -2,13 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 user_identifier = function() {
-    console.log("HERE");
     let user_info = Meteor.user();
-    if(user_info.username) return username;
-    if(user_info.profile) {
-        if(user_info.profile.name) return user_info.profile.name;
+    if(user_info) {
+      if(user_info.username) return username;
+      if(user_info.profile) {
+          if(user_info.profile.name) return user_info.profile.name;
+      }
     }
-    throw new Meteor.Error('No username found');
 }
 
 if(Meteor.isClient) {
