4811c1fcb7f8ec918d1485d303f03965540c4f7c
cypher <jp10010101010000@gmail.com>
2016-10-19 11:21:02 +1100

    Bug fix, wrong variable name

--- imports/api/helpers.js
+++ imports/api/helpers.js
@@ -4,7 +4,7 @@ import { Template } from 'meteor/templating';
 user_identifier = function() {
     let user_info = Meteor.user();
     if(user_info) {
-      if(user_info.username) return username;
+      if(user_info.username) return user_info.username;
       if(user_info.profile) {
           if(user_info.profile.name) return user_info.profile.name;
       }
